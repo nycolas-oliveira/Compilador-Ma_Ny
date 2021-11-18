b4d2e5b2d5ac30aadd80b14ebd62d2c13cadfda4
Matheus Bomfim Ferreira Fonseca <49158703+bomfim1209@users.noreply.github.com>
2021-11-17 22:52:32 -0300

    Update MeuParse
    Ajustando os comentários.

--- MeuParse.py
+++ MeuParse.py
@@ -1,42 +1,42 @@
 import sys
 from MeuLex import *
 
-# Parser object keeps track of current token, checks if the code matches the grammar, and emits code along the way.
+# O objeto analisador rastreia o token atual, verifica se o código corresponde à gramática e emite código ao longo do caminho.
 class Parser:
     def __init__(self, lexer, emitter):
         self.lexer = lexer
         self.emitter = emitter
 
-        self.symbols = set()    # All variables we have declared so far.
-        self.labelsDeclared = set() # Keep track of all labels declared
-        self.labelsGotoed = set() # All labels goto'ed, so we know if they exist or not.
+        self.symbols = set()    # Todas as variáveis que declaramos até agora.
+        self.labelsDeclared = set() # Mantem o controle de todos os rotulos. 
+        self.labelsGotoed = set() # Todos os rótulos foram acessados, então sabemos se eles existem ou não.
 
         self.curToken = None
         self.peekToken = None
         self.nextToken()
-        self.nextToken()    # Call this twice to initialize current and peek.
+        self.nextToken()    # Chame pela segunda vez para inicializar o peek atual.
 
-    # Return true if the current token matches.
+    # Retorna VERDADEIRO se o token atual corresponder.
     def checkToken(self, kind):
         return kind == self.curToken.kind
 
-    # Return true if the next token matches.
+    # Retorna VERDADEIRO se o próximo token corresponder.
     def checkPeek(self, kind):
         return kind == self.peekToken.kind
 
-    # Try to match current token. If not, error. Advances the current token.
+    # Tente combinar o token atual, caso contrário informa erro. Avança o token atual.
     def match(self, kind):
         if not self.checkToken(kind):
             self.abort("Esperado " + kind.name + ", Encontrado " + self.curToken.kind.name)
         self.nextToken()
 
-    # Advances the current token.
+    # Avança o token atual.
     def nextToken(self):
         self.curToken = self.peekToken
         self.peekToken = self.lexer.getToken()
-        # No need to worry about passing the EOF, lexer handles that.
+        # Não precisa se preocupar em passar o EOF, o lexer cuida disso.
 
-    # Return true if the current token is a comparison operator.
+    # Retorna VERDADEIRO se o token atual for um operador de comparação.
     def isComparisonOperator(self):
         return self.checkToken(TokenType.GT) or self.checkToken(TokenType.GTEQ) or self.checkToken(TokenType.LT) or self.checkToken(TokenType.LTEQ) or self.checkToken(TokenType.EQEQ) or self.checkToken(TokenType.NOTEQ)
 
@@ -44,51 +44,51 @@ class Parser:
         sys.exit("Erro! " + message)
 
 
-    # Production rules.
+    # Regras de produção.
 
-    # program ::= {statement}
+    # Programa ::= {declaração}
     def program(self):
         self.emitter.headerLine("#include <stdio.h>")
         self.emitter.headerLine("int main(void){")
         
-        # Since some newlines are required in our grammar, need to skip the excess.
+        # Uma vez que algumas novas linhas são necessárias em nossa gramática, é necessário pular o excesso.
         while self.checkToken(TokenType.NEWLINE):
             self.nextToken()
 
-        # Parse all the statements in the program.
+        # Analisa todas as instruções do programa.
         while not self.checkToken(TokenType.EOF):
             self.statement()
 
-        # Wrap things up.
+        # Embrulha 
         self.emitter.emitLine("return 0;")
         self.emitter.emitLine("}")
 
-        # Check that each label referenced in a GOTO is declared.
+        # Verifica se cada rótulo referenciado em um GOTO está declarado.
         for label in self.labelsGotoed:
             if label not in self.labelsDeclared:
                 self.abort("Attempting to GOTO to undeclared label: " + label)
 
 
-    # One of the following statements...
+    # Uma das seguintes afirmações ...
     def statement(self):
-        # Check the first token to see what kind of statement this is.
+        # Verifica o primeiro token para ver que tipo de declaração é.
 
-        # "PRINT" (expression | string)
+        # "PRINT" (expressão | fragmento)
         if self.checkToken(TokenType.PRINT):
             self.nextToken()
 
             if self.checkToken(TokenType.STRING):
-                # Simple string, so print it.
+                # String simples, imprima.
                 self.emitter.emitLine("printf(\"" + self.curToken.text + "\\n\");")
                 self.nextToken()
 
             else:
-                # Expect an expression and print the result as a float.
+                # Espera uma expressão e imprima o resultado como um float.
                 self.emitter.emit("printf(\"%" + ".2f\\n\", (float)(")
                 self.expression()
                 self.emitter.emitLine("));")
 
-        # "IF" comparison "THEN" block "ENDIF"
+        # "IF" comparação "THEN" bloqueia "ENDIF"
         elif self.checkToken(TokenType.IF):
             self.nextToken()
             self.emitter.emit("if(")
@@ -98,14 +98,14 @@ class Parser:
             self.nl()
             self.emitter.emitLine("){")
 
-            # Zero or more statements in the body.
+            # Zero ou mais declarações no corpo.
             while not self.checkToken(TokenType.ENDIF):
                 self.statement()
 
             self.match(TokenType.ENDIF)
             self.emitter.emitLine("}")
 
-        # "WHILE" comparison "REPEAT" block "ENDWHILE"
+        # "WHILE" comparação "REPEAT" bloqueia "ENDWHILE"
         elif self.checkToken(TokenType.WHILE):
             self.nextToken()
             self.emitter.emit("while(")
@@ -115,7 +115,7 @@ class Parser:
             self.nl()
             self.emitter.emitLine("){")
 
-            # Zero or more statements in the loop body.
+            # Zero ou mais instruções no corpo do loop.
             while not self.checkToken(TokenType.ENDWHILE):
                 self.statement()
 
@@ -126,7 +126,7 @@ class Parser:
         elif self.checkToken(TokenType.LABEL):
             self.nextToken()
 
-            # Make sure this label doesn't already exist.
+            # Certifca de que este rótulo ainda não exista.
             if self.curToken.text in self.labelsDeclared:
                 self.abort("Label already exists: " + self.curToken.text)
             self.labelsDeclared.add(self.curToken.text)
@@ -145,7 +145,7 @@ class Parser:
         elif self.checkToken(TokenType.LET):
             self.nextToken()
 
-            #  Check if ident exists in symbol table. If not, declare it.
+            #  Verifique se existe ident na tabela de símbolos. Se não, declare.
             if self.curToken.text not in self.symbols:
                 self.symbols.add(self.curToken.text)
                 self.emitter.headerLine("float " + self.curToken.text + ";")
@@ -161,12 +161,12 @@ class Parser:
         elif self.checkToken(TokenType.INPUT):
             self.nextToken()
 
-            # If variable doesn't already exist, declare it.
+            # Se a variável ainda não existe, declare-a.
             if self.curToken.text not in self.symbols:
                 self.symbols.add(self.curToken.text)
                 self.emitter.headerLine("float " + self.curToken.text + ";")
 
-            # Emit scanf but also validate the input. If invalid, set the variable to 0 and clear the input.
+            # Emita scanf, mas também valide a entrada. Se inválido, defina a variável como 0 e limpe a entrada.
             self.emitter.emitLine("if(0 == scanf(\"%" + "f\", &" + self.curToken.text + ")) {")
             self.emitter.emitLine(self.curToken.text + " = 0;")
             self.emitter.emit("scanf(\"%")
@@ -174,23 +174,24 @@ class Parser:
             self.emitter.emitLine("}")
             self.match(TokenType.IDENT)
 
-        # This is not a valid statement. Error!
+        # Se não é uma declaração válida. Erro!
         else:
             self.abort("Invalid statement at " + self.curToken.text + " (" + self.curToken.kind.name + ")")
 
-        # Newline.
+        # Nova linha.
         self.nl()
 
 
     # comparison ::= expression (("==" | "!=" | ">" | ">=" | "<" | "<=") expression)+
     def comparison(self):
         self.expression()
-        # Must be at least one comparison operator and another expression.
+
+        # Deve haver pelo menos um operador de comparação e outra expressão.
         if self.isComparisonOperator():
             self.emitter.emit(self.curToken.text)
             self.nextToken()
             self.expression()
-        # Can have 0 or more comparison operator and expressions.
+        # Pode ter 0 ou mais operadores e expressões de comparação.
         while self.isComparisonOperator():
             self.emitter.emit(self.curToken.text)
             self.nextToken()
@@ -200,7 +201,7 @@ class Parser:
     # expression ::= term {( "-" | "+" ) term}
     def expression(self):
         self.term()
-        # Can have 0 or more +/- and expressions.
+        # Pode ter 0 ou mais +/- e expressões.
         while self.checkToken(TokenType.PLUS) or self.checkToken(TokenType.MINUS):
             self.emitter.emit(self.curToken.text)
             self.nextToken()
@@ -210,7 +211,7 @@ class Parser:
     # term ::= unary {( "/" | "*" ) unary}
     def term(self):
         self.unary()
-        # Can have 0 or more *// and expressions.
+        # Pode ter 0 ou mais * // e expressões.
         while self.checkToken(TokenType.ASTERISK) or self.checkToken(TokenType.SLASH):
             self.emitter.emit(self.curToken.text)
             self.nextToken()
@@ -219,7 +220,7 @@ class Parser:
 
     # unary ::= ["+" | "-"] primary
     def unary(self):
-        # Optional unary +/-
+        # Unário opcional +/-
         if self.checkToken(TokenType.PLUS) or self.checkToken(TokenType.MINUS):
             self.emitter.emit(self.curToken.text)
             self.nextToken()        
@@ -232,7 +233,7 @@ class Parser:
             self.emitter.emit(self.curToken.text)
             self.nextToken()
         elif self.checkToken(TokenType.IDENT):
-            # Ensure the variable already exists.
+            # Certifique-se de que a variável já existe.
             if self.curToken.text not in self.symbols:
                 self.abort("Referencing variable before assignment: " + self.curToken.text)
 
@@ -244,8 +245,8 @@ class Parser:
 
     # nl ::= '\n'+
     def nl(self):
-        # Require at least one newline.
+        # Requer pelo menos uma nova linha.
         self.match(TokenType.NEWLINE)
-        # But we will allow extra newlines too, of course.
+        # Mas permitiremos novas linhas extras também, é claro.
         while self.checkToken(TokenType.NEWLINE):
             self.nextToken()
